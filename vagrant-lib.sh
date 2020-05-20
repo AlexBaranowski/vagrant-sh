c77c0648f15c2b6db0e8aae49561c300a6470a7d
Alex Baranowski <alex@euro-linux.com>
2020-05-20 09:45:03 +0200

    Add templates and more functions

--- vagrant-lib.sh
+++ vagrant-lib.sh
@@ -6,6 +6,7 @@
 
 preflight_check(){
     # This function checks if most important vars inplace
+    [ -n "$PROVIDER" ] && determine_provider
     vars_to_check=( PROVIDER BOX_NAME )
     for i in "${vars_to_check[@]}"; do
         [ -n "$i" ] || {
@@ -15,53 +16,78 @@ preflight_check(){
     done
 }
 
+determine_provider(){
+    # FIXME in the future more providers
+    if hash VirtualBox 2> /dev/null; then
+        echo "Found VirtualBox. Set Provider to VirtualBox"
+        PROVIDER=virtualbox
+    else
+        echo "Provider not found set libvirt!"
+        PROVIDER=libvirt
+    fi
+    export PROVIDER
+}
+
 vagrant_update_box(){
-    echo TODO
+    vagrant box update --box "$BOX_NAME" --provider "$PROVIDER"
 }
 
 vagrant_init(){
     # Strict mode
     set -euo pipefail
-    vagrant init "$BOX_NAME" "--provider=$PROVIDER"
-    echo TODO
+    vagrant init "$BOX_NAME"
+    
 }
 
 vagrant_init_from_template(){
-    echo TODO
+    [ -n "$TEMPLATE" ] && { 
+    echo "TEMPLATE not defined!"  && exit 1
+    }
+    vagrant init "$BOX_NAME" --template "$TEMPLATE"
 }
 
 vagrant_up(){
-    echo TODO
+    vagrant up --provider "$PROVIDER"
 }
 
 vagrant_destroy(){
-    echo TODO
+    set -euo pipefail
+    vagrant destroy -f
+    rm Vagrantfile
 }
 
 vagrant_remove_box(){
-    echo TODO
+    vagrant box remove -f "$BOX_NAME"
 }
 
 vagrant_run_command(){
-    echo TODO
+    vagrant ssh -c "$*"
 }
 
 vagrant_run_as_root(){
-    echo TODO
+    vagrant ssh -c "sudo $*"
 }
 
-vagrant_copy_files_from_machine(){
-    echo TODO
-}
-
-vagrant_destroy_all_machines(){
-    echo TODO
+vagrant_copy_file_from_machine(){
+    vagrant scp ":$*" .
 }
 
 vagrant_purge_libvirt_boxes(){
-    echo TODO
+    # vagrant-libvirt won't remove boxes images from libvirt default pool, so we have to do this manualy
+        # VAGRANTSLASH is string that is used to recognize from app.vagrantup.com
+    if [ $PROVIDER == 'libvirt' ]; then
+        echo "Libvirt provider require additonal cleaning"
+        for i in $(sudo virsh vol-list --pool default | grep "VAGRANTSLASH" | awk '{print $1}'); do
+        echo "Removing $i libvirt volume"
+        sudo virsh vol-delete --pool default $i
+        done
+    fi
 }
 
 vagrant_remove_all_boxes(){
-    echo TODO
+    for i in $( vagrant box list | awk '{print $1}' | sort | uniq ); do
+        echo "Purging box $i"
+        vagrant box remove -f --all $i
+    done
+    vagrant_purge_libvirt_boxes
 }
